e6e390483958b327a56003ee725fff3c963c4aff
ZIGOBOX <adelin.vignal.running@outlook.fr>
2026-08-13 23:17:27 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ function secureAppLogos(){
   });
 }
 
-const APP_VERSION='122.0';
-const APP_BUILD='13/08/2026 22:30';
+const APP_VERSION='124.0';
+const APP_BUILD='13/08/2026 22:46';
 
 // V25 : les erreurs techniques sont journalisées sans bloquer l'utilisateur.
 window.addEventListener('error',event=>{
